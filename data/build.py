7dd82cb5727958dc6c39a6056873fd041e7a9c8d
Andrew Stocker <sleepyams@gmail.com>
2023-03-10 14:14:34 -0700

    Update build.py

--- data/build.py
+++ data/build.py
@@ -4,12 +4,22 @@ from mido import MidiFile
 
 
 MIDI_FOLDER_ROOT = "adl-piano-midi"
+META_MESSAGE_SET_TEMPO = "set_tempo"
+
+
+def midi_tempo_to_bpm(micros_per_beat):
+    return 60_000_000 / micros_per_beat
 
 
 for root, _, files in os.walk(MIDI_FOLDER_ROOT):
     midi_files = [os.path.join(root, file) for file in files if file[-4:] == ".mid"]
 
-    for midi_file in midi_files:
-        midi = MidiFile(midi_file)
+    for filename in midi_files:
+        midi = MidiFile(filename)
         print(midi.filename, midi.length)
-        # print([msg for msg in midi if not msg.is_meta])
+        for meta_msg in [msg for msg in midi if msg.is_meta]:
+            if meta_msg.type == META_MESSAGE_SET_TEMPO:
+                print(midi_tempo_to_bpm(meta_msg.tempo))
+                break
+        else:
+            print("\tNo \"{}\" meta message!".format(META_MESSAGE_SET_TEMPO))
